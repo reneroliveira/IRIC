040eee20cae39e001c644d88ac39020dedc02a01
Rener-windows <rener.oliveira@fgv.edu.br>
2021-12-19 21:33:38 -0300

    adding base korean docs

---
+++ R/datasets.R
@@ -0,0 +1,10 @@
+#' Korean data documentation
+#'
+#' @name Korean
+#' @docType data
+#' @author Name \email{email@@provider}
+#' @references reference
+#' @usage data(Korean)
+#' @format A data frame with 26224 rows and 11 variables.
+#' @keywords data
+NULL
