a9d1749771488a1b2d17858c0cda624552435b18
Rener-windows <rener.oliveira@fgv.edu.br>
2021-12-19 21:36:02 -0300

    fixing namesÃpace

--- R/SPIDER.R
+++ R/SPIDER.R
@@ -9,6 +9,7 @@
 #' @return
 #' \item{newData}{A data frame after the application of SPIDER.}
 #' @references Stefanowski, J. and S. Wilk (2008). "Selective pre-processing of imbalanZced data for improving classification performance." Data Warehousing and Knowledge Discovery, Springer: 283-292.
+#' @importFrom caret createDataPartition
 #' @importFrom RANN nn2
 #' @importFrom parallel makeCluster stopCluster parLapply parSapply parApply
 #' @examples
